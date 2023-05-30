ed52fd3860a3ced9ce711a2979ecfdf1880b868d
Jonathan Wilkie <jwilki@lacitec.on.ca>
2023-05-30 16:57:49 -0400

    Revert metadata tests

--- app/(section)/module-1/benefice/page.jsx
+++ app/(section)/module-1/benefice/page.jsx
@@ -1,3 +1,6 @@
+/**
+ * @type {import("next").Metadata}
+ */
 export const metadata = {
     title: "Bénéfices du code source bien écrit",
     description: "Présentation des bénéfices et conséquence d'un code source bien organisé et bien écrit.",
