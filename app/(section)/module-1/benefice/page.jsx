104ab7ac6294e9c07d38060b66480b9a0d374ac5
Jonathan Wilkie <jwilki@lacitec.on.ca>
2023-05-30 16:48:49 -0400

    Trying to fix Github pipeline

--- app/(section)/module-1/benefice/page.jsx
+++ app/(section)/module-1/benefice/page.jsx
@@ -1,6 +1,3 @@
-/**
- * @type {import("next").Metadata}
- */
 export const metadata = {
     title: "Bénéfices du code source bien écrit",
     description: "Présentation des bénéfices et conséquence d'un code source bien organisé et bien écrit.",
